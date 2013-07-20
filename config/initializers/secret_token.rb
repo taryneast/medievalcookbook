2a9a24381eaca1c67e2f3e0bee3481cd8d6201c1
Taryn East <git@taryneast.org>
2013-07-20 11:03:48 +1000

    remove rails project so I can combine with heroku

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Recipes::Application.config.secret_token = '41d59008fec430e94b9880b64cfbd8911eef77e614578cb5f7955c8249e2eb888be2d775b2e3b05814c902d3f008aade23af6840c516efcc6be3820790772fa0'
