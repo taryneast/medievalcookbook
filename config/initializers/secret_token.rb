60b45ab8fa75295f92053688120ecfbdbef045d0
Taryn East <git@taryneast.org>
2013-07-20 11:02:08 +1000

    Add empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Medievalcookbook::Application.config.secret_key_base = 'fb63a10bfec732ce7d5014943b9cf2d202f9da463a854db1663ba00af1db647711ae93321d0f349a580fbb9521ef38d8dc016d6764d84a88d0a6286bcd2299e2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Medievalcookbook::Application.config.session_store :cookie_store, key: '_medievalcookbook_session'
