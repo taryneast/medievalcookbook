ac97bdac85c04618831cbfa4cf5efe078a132117
Taryn East <git@taryneast.org>
2013-09-30 18:33:42 +1000

    add some validation to my recipes
    just some simple presence-of validations.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,14 @@
 class Recipe < ActiveRecord::Base
+  validates :name, :ingredients, :steps, presence: true
+  validates :main_image_url, allow_blank: true, format: {
+      with: %r{\.(gif|jpg|jpeg|png)\Z}i,
+      message: 'must be a gif, jpg or png'
+    }
 
+  # a nicer way of displaying the two time-columns (if present)
   def display_time
-    return nil unless self.elapsed_time || self.prep_time
-    [self.elapsed_time,self.prep_time].reject(&:blank?).map(&:to_s).join('+')
+    return nil unless self.elapsed_time.present? || self.prep_time.present?
+    val = [self.prep_time,self.elapsed_time].reject(&:blank?).map(&:to_s).join('->')
+    "(#{val})"
   end
 end
--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -1,7 +1,51 @@
 require 'test_helper'
 
 class RecipeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  context "creating a recipe" do
+
+    should validate_presence_of(:name)
+    should validate_presence_of(:ingredients)
+    should validate_presence_of(:steps)
+
+    ['lalala.jpg','lalala.jpeg','something.gif','coolthing.png'].each do |good_url|
+      should allow_value(good_url).for(:main_image_url).with_message('must be a gif, jpg or png')
+    end
+    ['akywrgfgr3gb','jpg','badthing.jpg.pdf','evil.png/more'].each do |bad_url|
+      should_not allow_value(bad_url).for(:main_image_url).with_message('must be a gif, jpg or png')
+    end
+  end # context "creating a recipe"
+
+  context "instance methods" do
+    setup do
+      @recipe = Recipe.new
+    end
+    context "display_time" do
+      should "exist" do
+        assert @recipe.respond_to?(:display_time)
+      end
+      should "return nothing if neither time-column is present" do
+        @recipe.expects(:elapsed_time).returns(nil)
+        @recipe.expects(:prep_time).returns(nil)
+        assert_nil @recipe.display_time
+      end
+      should "return elapsed time only if it's the only one present" do
+        etime = 42
+        @recipe.expects(:elapsed_time).at_least_once.returns(etime)
+        @recipe.expects(:prep_time).at_least_once.returns(nil)
+        assert_equal "(#{etime})", @recipe.display_time
+      end
+      should "return prep time only if it's the only one present" do
+        ptime = 23
+        @recipe.expects(:elapsed_time).at_least_once.returns(nil)
+        @recipe.expects(:prep_time).at_least_once.returns(ptime)
+        assert_equal "(#{ptime})", @recipe.display_time
+      end
+      should "return a combined string if both present" do
+        etime,ptime = 42,23
+        @recipe.expects(:elapsed_time).at_least_once.returns(etime)
+        @recipe.expects(:prep_time).at_least_once.returns(ptime)
+        assert_equal "(#{ptime}->#{etime})", @recipe.display_time
+      end
+    end
+  end # context "instance methods"
 end
