3ea598f8ab39c95d2e3a546a4c9e709527649183
Taryn East <git@taryneast.org>
2013-10-01 18:38:32 +1000

    more substantial controller tests
    Recipe controller is where everything is at - so it pays to have more
    substantial controller testing over it.

--- test/controllers/cookbook_controller_test.rb
+++ test/controllers/cookbook_controller_test.rb
@@ -1,9 +1,28 @@
 require 'test_helper'
 
 class CookbookControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
-  end
+  fixtures :recipes
+
+  context "listing recipes" do
+    should "get index" do
+      get :index
+      assert_response :success
+    end
+    should "instantiate the recipes" do
+      get :index
+
+      arecipes = assigns(:recipes)
+      assert_not_nil arecipes, "should have instantiated the recipes"
+      assert_equal Recipe.count, arecipes.count, "should have all recipes instantiated"
+    end
+
+    context "the view" do
+      should "put a recipe table on the page" do
+        get :index
+        assert_response :success
+        assert_select "#columns #main .recipe", Recipe.count, "should have put a listing for each recipe on the page"
+      end
+    end # context "the view"
+  end # context "listing recipes"
 
 end
--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -5,39 +5,106 @@ class RecipesControllerTest < ActionController::TestCase
     @recipe = recipes(:one)
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:recipes)
-  end
+  context "listing recipes" do
+    should "get index" do
+      get :index
+      assert_response :success
+    end
+    should "instantiate the recipes" do
+      get :index
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
+      arecipes = assigns(:recipes)
+      assert_not_nil recipes, "should have instantiated the recipes"
+      assert_equal Recipe.count, arecipes.count, "should have all recipes instantiated"
+      assert arecipes.include?(@recipe), "should have put all the recipes in there, including the one we expect"
+    end
+    should "put a recipe table on the page" do
+      get :index
+      assert_response :success
+      assert_select "table tr", Recipe.count, "should have put a listing for each recipe on the page"
+    end
+  end # context "listing recipes"
 
-  test "should create recipe" do
-    assert_difference('Recipe.count') do
-      post :create, recipe: { description: @recipe.description, elapsed_time: @recipe.elapsed_time, equipment: @recipe.equipment, ingredients: @recipe.ingredients, main_image_url: @recipe.main_image_url, name: @recipe.name, original_source: @recipe.original_source, prep_time: @recipe.prep_time, steps: @recipe.steps }
+  context "creating recipes" do
+    should "get new" do
+      get :new
+      assert_response :success
+    end
+    should "instantiate a new recipe" do
+      get :new
+      the_recipe = assigns(:recipe)
+      assert_not_nil the_recipe
+      assert the_recipe.is_a?(Recipe)
+      assert the_recipe.new_record?, "should be a new instance"
     end
 
-    assert_redirected_to recipe_path(assigns(:recipe))
-  end
+    should "create a valid recipe" do
+      assert_difference('Recipe.count') do
+        post :create, recipe: { description: @recipe.description, elapsed_time: @recipe.elapsed_time, equipment: @recipe.equipment, ingredients: @recipe.ingredients, main_image_url: @recipe.main_image_url, name: @recipe.name, original_source: @recipe.original_source, prep_time: @recipe.prep_time, steps: @recipe.steps }
+      end
+
+      assert_redirected_to recipe_path(assigns(:recipe))
+    end
+
+    should "fail to create an invalid recipe" do
+      assert_no_difference('Recipe.count') do
+        post :create, recipe: { description: @recipe.description, elapsed_time: @recipe.elapsed_time, equipment: @recipe.equipment, ingredients: @recipe.ingredients, main_image_url: @recipe.main_image_url, name: nil, original_source: @recipe.original_source, prep_time: @recipe.prep_time, steps: @recipe.steps }
+
+      end
+
+      assert_response :success
+      assert render_template('new'), "should re-display the 'new' template"
+      assert assigns(:recipe).errors[:name].present?, "should have put the errors on the recipe"
+    end
+  end # context "creating recipes"
 
   test "should show recipe" do
     get :show, id: @recipe
     assert_response :success
   end
 
-  test "should get edit" do
-    get :edit, id: @recipe
-    assert_response :success
-  end
+  context "editing and exsting recipe" do
+    should "get edit" do
+      get :edit, id: @recipe
+      assert_response :success
+    end
+    should "find our recipe" do
+      get :edit, id: @recipe
 
-  test "should update recipe" do
-    patch :update, id: @recipe, recipe: { description: @recipe.description, elapsed_time: @recipe.elapsed_time, equipment: @recipe.equipment, ingredients: @recipe.ingredients, main_image_url: @recipe.main_image_url, name: @recipe.name, original_source: @recipe.original_source, prep_time: @recipe.prep_time, steps: @recipe.steps }
-    assert_redirected_to recipe_path(assigns(:recipe))
-  end
+      the_recipe = assigns(:recipe)
+      assert_not_nil the_recipe
+      assert_equal @recipe.id, the_recipe.id, "shoudl have found the one we asked for"
+    end
+
+
+    should "update a recipe with valid data" do
+      old_name = @recipe.name
+      new_name = old_name + '1234'
+      patch :update, id: @recipe, recipe: { description: @recipe.description, elapsed_time: @recipe.elapsed_time, equipment: @recipe.equipment, ingredients: @recipe.ingredients, main_image_url: @recipe.main_image_url, name: new_name, original_source: @recipe.original_source, prep_time: @recipe.prep_time, steps: @recipe.steps }
+      assert_redirected_to recipe_path(assigns(:recipe))
+    end
+    should "update our recipe" do
+      old_name = @recipe.name
+      new_name = old_name + '1234'
+
+      patch :update, id: @recipe, recipe: { description: @recipe.description, elapsed_time: @recipe.elapsed_time, equipment: @recipe.equipment, ingredients: @recipe.ingredients, main_image_url: @recipe.main_image_url, name: new_name, original_source: @recipe.original_source, prep_time: @recipe.prep_time, steps: @recipe.steps }
+
+      the_recipe = assigns(:recipe)
+      assert_not_nil the_recipe
+      assert_equal @recipe.id, the_recipe.id, "should have found the one we asked for"
+
+      @recipe.reload
+      assert_equal new_name, @recipe.name, "shoudl have persisted the name change"
+    end
+
+    should "fail to update a recipe with invalid data" do
+      patch :update, id: @recipe, recipe: { description: @recipe.description, elapsed_time: @recipe.elapsed_time, equipment: @recipe.equipment, ingredients: @recipe.ingredients, main_image_url: @recipe.main_image_url, name: nil, original_source: @recipe.original_source, prep_time: @recipe.prep_time, steps: @recipe.steps }
+
+      assert_response :success
+      assert render_template('edit'), "should re-display the 'edit' template"
+      assert assigns(:recipe).errors[:name].present?, "should have put the errors on the recipe"
+    end
+  end # context "editing and exsting recipe"
 
   test "should destroy recipe" do
     assert_difference('Recipe.count', -1) do
