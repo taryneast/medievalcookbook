fa619452c6ed6c18184cf0f65a77d12aa3ffded8
Taryn East <git@taryneast.org>
2013-10-09 10:04:32 +1100

    remove test for recipe-presences

--- test/models/step_test.rb
+++ test/models/step_test.rb
@@ -3,8 +3,5 @@ require 'test_helper'
 class StepTest < ActiveSupport::TestCase
   context "creating a step" do
     should belong_to(:recipe)
-
-    should validate_presence_of(:recipe)
   end
-
 end
